32952ed6fc99c5e9b7775d1c36494c483361f716
弓削善史 <yoshifumi@yoshi-bookpro.local>
2018-10-07 16:26:27 +0900

    p83 タスク削除処理

--- src/pages/task-list/task-list.ts
+++ src/pages/task-list/task-list.ts
@@ -35,7 +35,7 @@ export class TaskListPage {
     console.log('ionViewDidLoad TaskListPage');
   }
 
-  changeTask() {
+  changeTask(index: number) {
     let actionSheet = this.actionSheetCtrl.create({
       title: 'タスクの変更',
       buttons: [
@@ -43,7 +43,8 @@ export class TaskListPage {
           text: '削除',
           role: 'destructive',
           handler: () => {
-            console.log('Destructive clicked');
+            this.tasks.splice(index, 1);
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
           }
         },{
           text: '変更',
